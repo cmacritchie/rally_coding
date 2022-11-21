8bf42b8f5d1cd6ed12e030773cc858fdc3e9d8f5
cmacritchie <craig.macritchie@gmail.com>
2022-11-20 20:48:33 -0700

    vite project update

--- vite-project1/src/App.tsx
+++ vite-project1/src/App.tsx
@@ -1,32 +1,41 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import RQPage from './pages/ReactQuery'
+import Home from './pages/Home'
 
+const queryClient = new QueryClient()
+
+function App() {
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rqpage" element={<RQPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
+
+const Header = () => {
+  return(
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/rqpage">RQ Page</Link>
+      </nav>
     </div>
   )
 }
